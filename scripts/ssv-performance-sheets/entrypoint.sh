3368cd6c6622a6005957e8692820d3af4d7c2372
metanull-operator <54516310+metanull-operator@users.noreply.github.com>
2025-06-17 23:52:47 -0600

    Duplicated validator sheets into new dir

---
+++ scripts/ssv-performance-sheets/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -eo pipefail
+
+if [ -n "${CLICKHOUSE_PASSWORD_FILE:-}" ] && [ -f "$CLICKHOUSE_PASSWORD_FILE" ]; then
+  export CLICKHOUSE_PASSWORD=$(< "$CLICKHOUSE_PASSWORD_FILE")
+fi
+
+if [[ $# -eq 0 ]]; then
+  set -- python ssv-performance-sheets.py
+fi
+
+exec gosu sheets "$@"
